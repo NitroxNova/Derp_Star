d764f2e4e7eeadfef66b7beb1056b42bcc260ad0
NitroxNova <102335031+NitroxNova@users.noreply.github.com>
2022-03-24 21:47:13 -0500

    try again

--- Godot_Project/Bumper_Ships/Bio_Ship/dome.gd
+++
@@ -1,5 +0,0 @@
-extends CollisionShape2D
-
-func on_player_hit():
-	Player_Stats.add_points(100)
-	queue_free()
--- Godot_Project/Bumper_Ships/Bio_Ship/hospi-dome.gd
+++
@@ -1,5 +0,0 @@
-extends CollisionShape2D
-
-func on_player_hit():
-	Player_Stats.increment_health(10)
-	queue_free()
