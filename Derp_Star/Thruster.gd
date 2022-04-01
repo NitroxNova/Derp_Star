d33860ac6bf349b9cf904d233bde2195349ee84f
NitroxNova <102335031+NitroxNova@users.noreply.github.com>
2022-04-01 17:16:31 -0500

    thruster rotation and offset fix
    reverted rotation calculation to use global, changed offset to global position minus thruster global position

--- Derp_Star/Thruster.gd
+++ Derp_Star/Thruster.gd
@@ -12,5 +12,6 @@ func deactivate():
 	set_process(false)
 	
 func _process(delta):
-	var thrust_rot = Vector2(cos(rotation), sin(rotation)).normalized()
-	get_parent().apply_impulse(position,thrust_rot*delta*40)
+	var thrust_rot = Vector2(cos(global_rotation), sin(global_rotation)).normalized()
+	var thrust_offset = global_position - get_parent().global_position
+	get_parent().apply_impulse(thrust_offset,thrust_rot*delta*40)
