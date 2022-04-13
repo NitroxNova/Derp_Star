322ee076cbb29a62d4144c68b649f79244b6aa74
NitroxNova <102335031+NitroxNova@users.noreply.github.com>
2022-04-13 01:00:14 -0500

    add lifebar and death screen

--- main.gd
+++ main.gd
@@ -1,14 +1,33 @@
 extends Node2D
 
 var player_points = 0
+var max_health = 100
+var health = max_health
+
 signal update_points
+signal update_health
+signal update_max_health
+signal player_died
+
+func _process(delta):
+	minus_health(delta*10)
 
 func _init():
 	randomize()
 
+func _ready():
+	emit_signal("update_max_health",max_health)
+	emit_signal("update_health",health)
+
 func add_points(amount):
 	player_points += amount
 	emit_signal("update_points",player_points)
+
+func minus_health(amount):
+	health -= amount
+	emit_signal("update_health",health)
+	if health <= 0:
+		emit_signal("player_died")
 
 func _on_Derp_Star_body_entered(body):
 	if body.is_in_group("bumper"):
