961a0016dfbae1405bb070afd7dbc95f193ffd7b
NitroxNova <102335031+NitroxNova@users.noreply.github.com>
2022-04-13 00:58:16 -0500

    added life bar

---
+++ Derp_Star/Life_Bar.gd
@@ -0,0 +1,18 @@
+extends ProgressBar
+
+var half_life
+var styleBox = get("custom_styles/fg")
+var colors = [Color(1,0,0),Color(1,1,0),Color(0,1,0)]
+
+func set_max_value(max_health):
+	max_value = max_health
+	half_life = max_health/2
+
+func set_value(v):
+	var start = colors[floor(v/half_life)]
+	var end = colors[ceil(v/half_life)]
+	var percent = fmod(v,half_life)/half_life
+	var color = lerp(start,end,percent)
+	styleBox.bg_color = color
+	value = v
+
