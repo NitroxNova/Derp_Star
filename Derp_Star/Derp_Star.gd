3d67491fe65f0b555ccb428f2444e6e79092b486
NitroxNova <102335031+NitroxNova@users.noreply.github.com>
2022-04-16 00:53:40 -0500

    connect minus health

--- Derp_Star/Derp_Star.gd
+++ Derp_Star/Derp_Star.gd
@@ -3,6 +3,8 @@ extends RigidBody2D
 var thrusters = []
 var hotkeys = ["thruster_1","thruster_2","thruster_3","thruster_4","thruster_5"]
 
+signal minus_health
+
 func _ready():
 	thrusters = [$Thruster1,$Thruster2,$Thruster3,$Thruster4,$Thruster5]
 
@@ -12,3 +14,6 @@ func _process(delta):
 			thrusters[i].activate()
 		if Input.is_action_just_released(hotkeys[i]):
 			thrusters[i].deactivate()
+
+func minus_health(amount):
+	emit_signal("minus_health",amount)
