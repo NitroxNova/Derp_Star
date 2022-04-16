4be9c062dc1da5b49d25d9c118d9a756019fabad
NitroxNova <102335031+NitroxNova@users.noreply.github.com>
2022-04-15 23:23:51 -0500

    add laser

---
+++ Bumper_Ships/laser/Laser.gd
@@ -0,0 +1,13 @@
+extends RigidBody2D
+
+func _ready():
+	linear_velocity = Vector2(700,0).rotated(rotation)
+
+func _on_Despawn_Timer_timeout():
+	queue_free()
+
+func _on_Laser_body_entered(body):
+	if body.is_in_group("player"):
+		body.minus_health(5)
+		queue_free()
+	$Sprite.look_at(linear_velocity+position)
