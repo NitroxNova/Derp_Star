5dce6b6e9c2676e42e540fdde28e27b56b46fc0c
NitroxNova <102335031+NitroxNova@users.noreply.github.com>
2022-04-21 18:01:40 -0500

    fix integral

--- Derp_Star/Life_Bar.gd
+++ Derp_Star/Life_Bar.gd
@@ -9,8 +9,8 @@ func set_max_value(max_health):
 	half_life = max_health/2
 
 func set_value(v):
-	var start = colors[floor(v/half_life)]
-	var end = colors[ceil(v/half_life)]
+	var start = colors[floor(float(v)/half_life)]
+	var end = colors[ceil(float(v)/half_life)]
 	var percent = fmod(v,half_life)/half_life
 	var color = lerp(start,end,percent)
 	styleBox.bg_color = color
