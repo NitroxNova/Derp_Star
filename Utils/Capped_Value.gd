01c0bd814defa2a0b368e3e589328d33885129a9
NitroxNova <102335031+NitroxNova@users.noreply.github.com>
2022-04-24 14:13:41 -0500

    add capped value resource

---
+++ Utils/Capped_Value.gd
@@ -0,0 +1,31 @@
+extends Resource
+class_name Capped_Value
+
+export (int) var current
+export (int) var maximum
+
+signal current_changed
+signal current_zero
+signal maximum_changed
+
+func set_current(amount):
+	if current > 0 and amount <= 0:
+		emit_signal("current_zero")
+	current = min(amount,maximum)
+	emit_signal("current_changed",current)
+
+func increase_current(amount):
+	set_current(current + amount)
+
+func decrease_current(amount):
+	set_current(current - amount)
+
+func set_maximum(amount):
+	maximum = amount
+	emit_signal("maximum_changed", maximum)
+
+func increase_maximum(amount):
+	set_maximum(maximum + amount)
+
+func get_percent():
+	return float(current)/maximum
