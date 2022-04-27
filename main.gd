d20d3fe309e6b7ec8366e1facff288df881026cf
NitroxNova <102335031+NitroxNova@users.noreply.github.com>
2022-04-27 00:44:52 -0500

    refactor health

--- main.gd
+++ main.gd
@@ -1,37 +1,21 @@
 extends Node2D
 
 var player_points = 0
-var max_health = 100
-var health = max_health
 
 signal update_points
-signal update_health
-signal update_max_health
-signal player_died
 
-	
 func _init():
 	randomize()
-
+	
 func _ready():
-	emit_signal("update_max_health",max_health)
-	emit_signal("update_health",health)
+	$Derp_Star.health.update_maximum()
+	$Derp_Star.health.update_current()
+	$Derp_Star/Muh_Lazer.energy.update_maximum()
+	$Derp_Star/Muh_Lazer.energy.update_current()
 
 func add_points(amount):
 	player_points += amount
 	emit_signal("update_points",player_points)
-
-func minus_health(amount):
-	health -= amount
-	emit_signal("update_health",health)
-	if health <= 0:
-		emit_signal("player_died")
-
-func _on_Derp_Star_body_entered(body):
-	if body.is_in_group("bumper"):
-		add_points(50)
-		body.queue_free()
-		update_bumper_count()
 
 func spawn_bumper(bumper):
 	bumper.connect("add_points",self,"add_points")
