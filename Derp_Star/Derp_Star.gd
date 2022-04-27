64ecf8732849e9ada0d68df757ed6a049d30f3da
NitroxNova <102335031+NitroxNova@users.noreply.github.com>
2022-04-27 00:49:16 -0500

    refactor health, collision damage

--- Derp_Star/Derp_Star.gd
+++ Derp_Star/Derp_Star.gd
@@ -3,7 +3,16 @@ extends RigidBody2D
 onready var thrusters = [$Thruster1,$Thruster2,$Thruster3,$Thruster4,$Thruster5]
 var hotkeys = ["thruster_1","thruster_2","thruster_3","thruster_4","thruster_5"]
 
-signal minus_health
+export (Resource) var health
+
+signal update_health
+signal update_max_health
+signal player_died
+
+func _ready():
+	health.connect("current_changed",self,"update_health")
+	health.connect("maximum_changed",self,"update_max_health")
+	health.connect("current_zero",self,"player_died")
 
 func _process(delta):
 	for i in 5:
@@ -18,6 +27,19 @@ func _input(event):
 			$Muh_Lazer.activate()
 		else:
 			$Muh_Lazer.deactivate()
+			
+func _on_Derp_Star_body_entered(body):
+	if body.is_in_group("bumper"):
+		body.take_damage(linear_velocity.length()/10)
+				
+func take_damage(amount):
+	health.decrease_current(amount)
 
-func minus_health(amount):
-	emit_signal("minus_health",amount)
+func update_health(amount):
+	emit_signal("update_health", amount)
+	
+func update_max_health(amount):
+	emit_signal("update_max_health", amount)
+
+func player_died():
+	emit_signal("player_died")
