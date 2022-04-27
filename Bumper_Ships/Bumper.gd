a319abc681005fcc33ed8aeb9b56e116ef4cc2a0
NitroxNova <102335031+NitroxNova@users.noreply.github.com>
2022-04-27 00:39:08 -0500

    export health, update health bar

--- Bumper_Ships/Bumper.gd
+++ Bumper_Ships/Bumper.gd
@@ -1,13 +1,21 @@
 class_name Bumper_Ship
 extends RigidBody2D
 
-var health = 75
-var points = 50
+export (Resource) var health
+export (int) var points
 
 signal add_points
 
+func setup():
+	health.connect("current_zero",self,"died")
+	$Health_Node/Node2D/ProgressBar.value = health.current
+	$Health_Node/Node2D/ProgressBar.max_value = health.maximum
+
 func take_damage(amount):
-	health -= amount
-	if health <= 0:
-		queue_free()
-		emit_signal("add_points",points)
+	health.decrease_current(amount)
+	$Health_Node/Node2D/ProgressBar.value = health.current
+	$Health_Node/Node2D/ProgressBar.show()
+	
+func died():
+	emit_signal("add_points",points)
+	queue_free()
