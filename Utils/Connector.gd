60564137a66d311386ad7e189f7cf2d0ce7b0d21
NitroxNova <102335031+NitroxNova@users.noreply.github.com>
2022-07-04 22:50:23 -0500

    add draw explosion to connector

--- Utils/Connector.gd
+++ Utils/Connector.gd
@@ -1,9 +1,14 @@
 extends Node
 
 var derp_star
+var explosions
 
 func reset():
 	derp_star = get_node("/root/Main/Derp_Star")
+	explosions = get_node("/root/Main/Explosions")
 
 func derp_star_position(node):
 	derp_star.connect("position_changed",node,"derp_star_position_changed")
+
+func draw_explosion(node):
+	explosions.add_child(node)
