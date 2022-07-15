872ecd4a01e3658ee80276c593990aad80bf21aa
NitroxNova <102335031+NitroxNova@users.noreply.github.com>
2022-07-14 21:05:55 -0500

    use animation player to change color trail

--- Bumper_Ships/Shooting_Star/Bumper.gd
+++ Bumper_Ships/Shooting_Star/Bumper.gd
@@ -16,12 +16,6 @@ func shoot():
 	b.transform = $Laser_Pointer.global_transform
 	get_tree().get_root().add_child(b)
 
-func change_color():
-	var offsets = $Rainbow.process_material.color_ramp.gradient.offsets
-	for offset in offsets.size():
-		offsets[offset] = fmod(offsets[offset] + 1.05, float(offsets.size())) - 1
-	$Rainbow.process_material.color_ramp.gradient.offsets = offsets
-
 func _on_Shooting_Star_body_entered(body):
 	var multiplier = MAX_SPEED/linear_velocity.length()
 	linear_velocity *= multiplier
