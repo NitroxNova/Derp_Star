efe5404b7b80fe0e5ba8ce698c4885ec95fec02f
NitroxNova <102335031+NitroxNova@users.noreply.github.com>
2022-07-19 11:54:02 -0500

    load dimension

--- Utils/Connector.gd
+++ Utils/Connector.gd
@@ -6,14 +6,23 @@ var explosions
 var pick_ups
 var overlay
 var hud
+var dimension_list
 
 func reset():
 	derp_star = get_node("/root/Main/Derp_Star")
-	explosions = get_node("/root/Main/Explosions")
-	pick_ups = get_node("/root/Main/Pick_Ups")
+	explosions = get_node("/root/Main/Dimension/Explosions")
+	pick_ups = get_node("/root/Main/Dimension/Pick_Ups")
 	overlay = get_node("/root/Main/Overlay")
 	hud = get_node("/root/Main/Overlay/HUD")
 	main = get_node("/root/Main")
+	dimension_list = []
+	dimension_list.append(get_node("/root/Main/Dimension"))
+	dimension_list.append(load("res://Dimension/Olympus/Dimension.tscn").instance())
+
+func load_dimension(id):
+	var cur_dim = get_node("/root/Main/Dimension")
+	main.remove_child(cur_dim)
+	main.add_child(dimension_list[id])
 
 func derp_star_position(node):
 	derp_star.connect("position_changed",node,"derp_star_position_changed")
