1f928cfb2a5f7a4b587e5fb10be116ce0bcc8ba8
NitroxNova <102335031+NitroxNova@users.noreply.github.com>
2022-07-22 17:56:40 -0500

    detect player shield, add damage source tag

--- Bumper_Ships/Laser/Laser.gd
+++ Bumper_Ships/Laser/Laser.gd
@@ -1,5 +1,8 @@
 extends Enemy_Projectile
 
+var source = "bumper"
+var damage = 3
+
 func _ready():
 	linear_velocity = Vector2(700,0).rotated(rotation)
 
@@ -7,7 +10,14 @@ func _on_Despawn_Timer_timeout():
 	queue_free()
 
 func _on_Laser_body_entered(body):
-	if body.is_in_group("player"):
-		body.take_damage(3)
+	if body.is_in_group("player") and source == "bumper":
+		body.take_damage(damage)
+		queue_free()
+	elif body.is_in_group("Player_Shield"):
+		source = "player"
+		body.take_damage(damage)
+	elif source == "player":
+		body.take_damage(damage)
 		queue_free()
 	$Sprite.look_at(linear_velocity+position)
+	
