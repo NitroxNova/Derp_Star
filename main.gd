4b33864e379d2d9ed4cfd61706d86ae4e7d067b0
NitroxNova <102335031+NitroxNova@users.noreply.github.com>
2022-08-09 22:40:04 -0500

    talents and points for testing, will revert later

--- main.gd
+++ main.gd
@@ -6,8 +6,12 @@ func _init():
 func _ready():
 	Save.load_file()
 	Connector.reset()
-#	Player_Stats.increase_points(10000)
-#	Player_Stats.increase_boss_cores(20)
+	Player_Stats.increase_points(10000)
+	Player_Stats.increase_boss_cores(20)
+	Player_Stats.talent["laser_beam"].increase_upgrade()
+	Player_Stats.talent["energy_shield"].increase_upgrade()
+	Player_Stats.talent["gravity_beam"].increase_upgrade()
+
 	$Derp_Star.health.update_maximum()
 	$Derp_Star.health.update_current()
 	$Derp_Star.energy.update_maximum()
