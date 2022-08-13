334955b1a0685f11dd39bef98f75e4a717d531ab
NitroxNova <102335031+NitroxNova@users.noreply.github.com>
2022-08-13 17:01:12 -0500

    add cloud bumper

---
+++ Bumper_Ships/Olympus_Cloud/Bumper.gd
@@ -0,0 +1,22 @@
+extends Bumper_Ship
+
+var lightning = preload("res://Bumper_Ships/Olympus_Cloud/Bolt.tscn")
+enum {HAPPY,ANGRY}
+var state = HAPPY
+
+func take_damage(amount):
+	.take_damage(amount)
+	if state != ANGRY:
+		$Base_Sprite.show()
+		$Happy.show()
+		$Lightning_Timer.start()
+		state = ANGRY
+
+func _on_Lightning_Timer_timeout():
+	$Lightning_Rod.look_at(Connector.derp_star.global_position)
+	var spread = .5
+	var rot = (randf() * spread) - (spread/2.0)
+	$Lightning_Rod.rotate(rot)
+	var l = lightning.instance()
+	l.transform = $Lightning_Rod/Spawn.global_transform
+	Connector.spawn_projectile(l)
