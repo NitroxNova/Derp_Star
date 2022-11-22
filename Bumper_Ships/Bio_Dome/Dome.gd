9a432c8226b48582dee6bc2b3fcd4534bc20ef9e
NitroxNova <102335031+NitroxNova@users.noreply.github.com>
2022-11-21 19:44:46 -0600

    was breaking on dimension reload

--- Bumper_Ships/Bio_Dome/Dome.gd
+++ Bumper_Ships/Bio_Dome/Dome.gd
@@ -25,5 +25,4 @@ func make_joints():
 		var joint = PinJoint2D.new()
 		joint.node_a = ".."
 		joint.node_b = "../../" + tube.name
-		tube.connect("tree_exited",joint,"queue_free")
 		add_child(joint)
