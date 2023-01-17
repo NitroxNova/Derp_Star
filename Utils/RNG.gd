fe37f5b7296214bcb5500f42238a2f4f85b7dc4e
NitroxNova <102335031+NitroxNova@users.noreply.github.com>
2023-01-17 17:48:20 -0600

    add array_rand_multi function

--- Utils/RNG.gd
+++ Utils/RNG.gd
@@ -15,3 +15,12 @@ func randf_range(from,to):
 func array_rand(arr):
 	var index = rng.randi_range(0,arr.size()-1)
 	return arr[index]
+
+func array_rand_multi(in_array, count):
+	var out_array = []
+	while count > 0:
+		var value = array_rand(in_array)
+		out_array.append(value)
+		in_array.erase(value)
+		count -= 1
+	return out_array
