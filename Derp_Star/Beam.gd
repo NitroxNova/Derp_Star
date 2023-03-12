2ca4835efcfc19e37463a6b2ad2c9f192759a627
NitroxNova <102335031+NitroxNova@users.noreply.github.com>
2023-03-12 08:36:03 -0500

    toggle gravity every frame to fix orbiting bumpers

--- Derp_Star/Beam.gd
+++ Derp_Star/Beam.gd
@@ -3,6 +3,8 @@ extends Node2D
 class_name Player_Beam
 
 @export var energy_cost : int
+var gravity_toggle : bool = true
+@onready var initial_collision_mask = $Beam.collision_mask
 
 signal decrease_energy
 
@@ -12,13 +14,20 @@ func _ready():
 func _process(delta):
 	look_at(get_global_mouse_position())
 	emit_signal("decrease_energy",energy_cost * delta)
+	if gravity_toggle:
+		$Beam.collision_mask = 0
+	else:
+		$Beam.collision_mask = initial_collision_mask
+	gravity_toggle = not gravity_toggle
 	
 func activate():
 	$Beam.show()
 	$Beam/CollisionShape2D.disabled = false
-	set_process(true)
+	process_mode = Node.PROCESS_MODE_INHERIT
+	$Beam.collision_mask = initial_collision_mask
 	
 func deactivate():
 	$Beam.hide()
 	$Beam/CollisionShape2D.disabled = true
-	set_process(false)
+	process_mode = Node.PROCESS_MODE_DISABLED
+	$Beam.collision_mask = 0
