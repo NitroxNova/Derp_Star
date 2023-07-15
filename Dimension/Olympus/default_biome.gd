9fcc571f915ad29ec6803b15102788443ecc53aa
NitroxNova <102335031+NitroxNova@users.noreply.github.com>
2023-07-14 22:55:52 -0500

    snowflake to olympus bumper list

--- Dimension/Olympus/default_biome.gd
+++ Dimension/Olympus/default_biome.gd
@@ -3,3 +3,4 @@ extends Biome
 func _init():
 	bumper_list.append(load("res://Bumper_Ships/Olympus_Cloud/Bumper.tscn"))
 	bumper_list.append(load("res://Bumper_Ships/Hot_Air_Balloon/Bumper.tscn"))
+	bumper_list.append(load("res://Bumper_Ships/Snowflake/bumper.tscn"))
